a11e7bd0e69de946133df1593cffff80eb73ff2a
Ronny019 <mainak.pc@gmail.com>
2024-06-08 19:09:01 -0600

    sign in and form field

--- Aora/app/(auth)/sign-in.jsx
+++ Aora/app/(auth)/sign-in.jsx
@@ -2,10 +2,15 @@ import { useState } from "react";
 import { Link, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
+import FormField from "../../components/FormField";
 
 import { images } from "../../constants";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -25,7 +30,19 @@ const SignIn = () => {
           <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
             Log in to Aora
           </Text>
-
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
