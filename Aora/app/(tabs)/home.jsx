e782aac80e8bd7e1fb3f53bf828c0194c4d598ba
Ronny019 <mainak.pc@gmail.com>
2024-07-30 14:32:52 -0600

    home page

--- Aora/app/(tabs)/home.jsx
+++ Aora/app/(tabs)/home.jsx
@@ -1,12 +1,44 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import {images} from '../../constants'
 const home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-primary">
       <FlatList 
-        
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+      />
+      )}
+      ListHeaderComponent={() => (
+        <View className="flex my-6 px-4 space-y-6">
+          <View className="flex justify-between items-start flex-row mb-6">
+            <View>
+              <Text className="font-pmedium text-sm text-gray-100">
+                Welcome Back
+              </Text>
+              <Text className="text-2xl font-psemibold text-white">
+                JSMastery
+              </Text>
+            </View>
+
+            <View className="mt-1.5">
+              <Image
+                source={images.logoSmall}
+                className="w-9 h-10"
+                resizeMode="contain"
+              />
+            </View>
+          </View>
+          </View>
+      )}
       />
     </SafeAreaView>
   )
